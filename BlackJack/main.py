bf413aabf90b71759adb5e5b95e663cd3a60ea83
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-07 13:50:18 +0000

    Refining main

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -1,6 +1,5 @@
 from blackjack import PlayBlackJack, FinalHand, EndGame
 from logo import logo
-from random import randint
 def main() -> None:
 
     player_turn = PlayBlackJack()
@@ -9,6 +8,7 @@ def main() -> None:
     end_the_game = EndGame()
     final = FinalHand()
     is_playing = True
+    user_wants_another = True
 
     while is_playing:
         player_cards = []
@@ -20,28 +20,34 @@ def main() -> None:
             for _ in range(2):
                 player_turn.scores_lst(player_cards, player_turn.play_blackjack())
                 games_turn.scores_lst(games_cards, games_turn.play_blackjack())
+
 # Display the player cards and their total and Computer card
             print(play_game.display_game_status(player_cards, games_cards))
-            another_card=input(f"Type 'y' to get another card,  type 'n' to pass. ")
+            while user_wants_another:
+                another_card=input(f"Type 'y' to get another card,  type 'n' to pass. ")
 
+                if another_card.lower() == 'y':
 
-            if another_card.lower() == 'y':
 # Draw another card for the player
-                player_turn.scores_lst(player_cards, player_turn.play_blackjack())
-# get the Computer's score
-                games_score = games_turn.current_score(games_cards)
+                    player_turn.scores_lst(player_cards, player_turn.play_blackjack())
+
+# Display the player cards and their total and Computer card
+                    print(play_game.display_game_status(player_cards, games_cards))
 
 # Player doesn't want another card
-            elif another_card.lower() == "n":
+                elif another_card.lower() == "n":
+                    user_wants_another = False
+
 # get the games score
-                games_score = games_turn.current_score(games_cards)
-                while games_score < 17 and games_score != 0:
+            games_score = games_turn.current_score(games_cards)
+            player_score = player_turn.current_score(player_cards)
+            while games_score < 17 and games_score != 0 and player_score != 0:
+
 # draw more cards for the computer if computer score is less than 17
-                        games_turn.scores_lst(games_cards, games_turn.play_blackjack())
-                        games_score = games_turn.current_score(games_cards)
+                games_turn.scores_lst(games_cards, games_turn.play_blackjack())
+                games_score = games_turn.current_score(games_cards)
 
 #get player scores and games score
-            player_score = player_turn.current_score(player_cards)
             games_score = games_turn.current_score(games_cards)
 
             print(end_the_game.end_game(games_cards,player_cards,player_score, games_score))
